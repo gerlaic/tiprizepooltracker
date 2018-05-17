cc6948328912b84dffbaf1c82455574d7e1338a1
Bing Gan <gan_bing@vpgame.cn>
2018-05-17 01:39:44 -0700

    added config file and logic to send into db

--- ti8scrapper.py
+++ ti8scrapper.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import configparser
+Config = configparser.ConfigParser()
+import mysql.connector
 
 class Ti8scrapperSpider(scrapy.Spider):
     name = 'ti8scrapper'
@@ -13,5 +15,32 @@ class Ti8scrapperSpider(scrapy.Spider):
         prize_num = prizepool.replace(',','')[1:]
         # print(prize_num)
         # sending to db
+        ## read config file
+        conf_file = Config.read("config.ini")
+        host = Config.get("DB-info", "host")
+        user = Config.get("DB-info", "user")
+        password = Config.get("DB-info", "password")
+        database = Config.get("DB-info", "database")
+        table = Config.get("DB-info", "table")
+
+        cmd = ("insert into {0} "
+            "(column1, column2, column3, ...)"
+            "values "
+            "(value1, value2, value3, ...)".format(table))
         
+        config = {'host' : host,
+                'user' : user,
+                'password' : password,
+                'database' : database}
+        try:
+            cnx = mysql.connector.connect(host=host,user=user,
+                                        password=password,database=database)
+        except mysql.connector.errors.InterfaceError as e:
+            print("DB Interface Error")
+        pass
+        cursor = cnx.cursor()
+        cursor.execute(cmd)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
         pass
